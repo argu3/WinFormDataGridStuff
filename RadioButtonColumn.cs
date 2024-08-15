56469f4d3605431c28f2b97d9ad2c190ceccc66e
Andrew Guthrie <93886464+argu3@users.noreply.github.com>
2024-08-15 13:09:12 -0400

    Add files via upload

---
+++ RadioButtonColumn.cs
@@ -0,0 +1,265 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+*/
+namespace AssetStatusInfo
+{
+    internal class RadioButtonColumn
+    {
+        private List<string> checkBoxColumnNames = new List<string>(); //unused 
+        private Dictionary<string, List<string>> tablesWithCheckboxColumns = new Dictionary<string, List<string>>();
+        private Dictionary<string, int> selectedCell = new Dictionary<string, int>();
+        //coloring 
+        private int penThickness = 2; 
+        private Brush borderColor = Brushes.Black;
+        private Brush fillColor = Brushes.LightBlue;
+        public Dictionary<string, int> SelectedCell
+        {
+            get => selectedCell;
+        }
+        public RadioButtonColumn()
+        {
+
+        }
+        public RadioButtonColumn(DataGridViewCheckBoxColumn checkboxColumn, int PenThickness, Brush BorderColor, Brush FillColor)
+        {
+            checkBoxColumnNames.Add(checkboxColumn.Name);
+            penThickness = PenThickness;
+            borderColor = BorderColor;
+            fillColor = FillColor;
+        }
+        public RadioButtonColumn(List<DataGridViewCheckBoxColumn> CheckBoxColumn, int PenThickness, Brush BorderColor, Brush FillColor)
+        {
+            CheckBoxColumn.ForEach(checkboxColumn => { checkBoxColumnNames.Add(checkboxColumn.Name); });
+            penThickness = PenThickness;
+            borderColor = BorderColor;
+            fillColor = FillColor;
+        }
+
+        public void ConvertToRadioButtonColumn(DataGridView grid, DataGridViewCheckBoxColumn checkboxColumn)
+        //single point of entry for adding all event listeners 
+        {
+            if (checkboxColumn.Name != "") //if checkboxColumnName is not specified, all checkBox columns become radio button columns
+            {
+                checkBoxColumnNames.Add(checkboxColumn.Name);
+                selectedCell[checkboxColumn.Name] = -1;
+                List<string>? test;
+                tablesWithCheckboxColumns.TryGetValue(grid.Name, out test);
+                if (test != null)
+                {
+                    tablesWithCheckboxColumns[grid.Name].Add(checkboxColumn.Name);
+                }
+                else
+                {
+                    tablesWithCheckboxColumns[grid.Name] = new List<string> { checkboxColumn.Name };
+                }
+            }
+            grid.CellEndEdit += this.StateCheckOnCellEndEdit; 
+            grid.CurrentCellDirtyStateChanged += this.DeselectPreviousOption;
+            grid.CellPainting += this.RadioButtonPainter;
+            grid.DataSourceChanged += Grid_DataSourceChanged;
+            grid.CellMouseClick += Grid_CellMouseClick;
+            grid.DataBindingComplete += Grid_DataSourceChanged;
+        }
+
+        private void Grid_CellMouseClick(object? sender, DataGridViewCellMouseEventArgs e)
+        //checks for click within the drawn circle
+        {
+            var valid = ValidateColumn(sender, e.ColumnIndex);
+            if (valid.valid && e.ColumnIndex > -1 && e.RowIndex > -1)
+            {
+                int width = valid.grid.Rows[e.RowIndex].Cells[e.ColumnIndex].Size.Width;
+                int height = valid.grid.Rows[e.RowIndex].Cells[e.ColumnIndex].Size.Height;
+                int xCell = valid.grid.Rows[e.RowIndex].Cells[e.ColumnIndex].ContentBounds.X;
+                int yCell = valid.grid.Rows[e.RowIndex].Cells[e.ColumnIndex].ContentBounds.Y;
+                int xClick = e.Location.X;
+                int yClick = e.Location.Y;
+                int radius;
+                if (width < height)
+                {
+                    radius = width;
+                }
+                else
+                {
+                    radius = height;
+                }
+                if(((xClick - xCell)*(xClick - xCell)+ (yClick - yCell)* (yClick - yCell)) < ((radius / 2)*(radius / 2)))
+                {
+                    valid.grid.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = true;
+                    DeselectPreviousOption(sender, e);
+                }
+            }
+        }
+        private void Grid_DataSourceChanged(object? sender, EventArgs e)
+        //if I'm inserting values into the grid I need to adjust the selected cell
+        {
+            var valid = ValidateColumn(sender, -1);
+            if (valid.grid != null)
+            {
+                DataGridView s = valid.grid;
+                List<int> checkBoxColumns = new List<int>();
+                for (int i = 0; i < s.Columns.Count; i++)
+                {
+                    if (ValidateColumn(sender, i).valid)
+                    {
+                        if (selectedCell[s.Columns[i].Name] > -1)
+                        {
+                                selectedCell[s.Columns[i].Name] = -1; //if nothing is found 
+                                for (int j = 0; j < s.RowCount; j++)
+                                {
+                                    if (s.Rows[j].Cells[i].Value == null ? false : (bool)s.Rows[j].Cells[i].Value == true)
+                                    {
+                                        selectedCell[s.Columns[i].Name] = j;
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void RadioButtonPainter(object? sender, DataGridViewCellPaintingEventArgs e)
+        {
+            var valid = ValidateColumn(sender, e.ColumnIndex);
+            if (valid.valid && e.RowIndex != -1)
+            {
+                DataGridView s = valid.grid;
+                e.Handled = true;
+                //Rectangle b = new Rectangle();
+                Pen pen = new Pen(borderColor, penThickness);
+                Size size = e.CellBounds.Size;
+                if (size.Width < size.Height)
+                {
+                    size.Width = size.Width - penThickness - 3 * penThickness;
+                    size.Height = size.Width;
+                }
+                else
+                {
+                    size.Height = size.Height - penThickness - 3 * penThickness;
+                    size.Width = size.Height;
+                }
+                Point point = new Point(e.CellBounds.X + (e.CellBounds.Width - size.Width) / 2, e.CellBounds.Y + (e.CellBounds.Height - size.Height) / 2); //fix misalignment with interior dot
+                Rectangle rectangle = new Rectangle(point, size);
+                e.PaintBackground(rectangle, false);
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                e.Graphics.DrawEllipse(pen, rectangle);
+                rectangle.X += 2 * penThickness;
+                rectangle.Y += 2 * penThickness;
+                rectangle.Width = size.Width - 4 * penThickness;
+                rectangle.Height = size.Height - 4 * penThickness;
+                bool cellValue = true;
+                if (s.CurrentCell.RowIndex == e.RowIndex && s.CurrentCell.ColumnIndex == e.ColumnIndex) //no other way to check if it's dirty
+                {
+                    if (s.CurrentCell.Value == null) { cellValue = false; }
+                    else { cellValue = (bool)s.CurrentCell.Value; }
+                    if ((s.IsCurrentCellDirty == true && cellValue == false) || cellValue)
+                    {
+                        pen.Brush = fillColor;
+                        e.Graphics.DrawEllipse(pen, rectangle);
+                        e.Graphics.FillEllipse(fillColor, rectangle);
+                    }
+                    else
+                    {
+                        pen.Color = s.Rows[e.RowIndex].DefaultCellStyle.BackColor;
+                    }
+                }
+                else if (e.Value != null)
+                {
+                    if ((bool)e.Value == true)
+                    {
+                        pen.Brush = fillColor;
+                        e.Graphics.DrawEllipse(pen, rectangle);
+                        e.Graphics.FillEllipse(fillColor, rectangle);
+                    }
+                    else
+                    {
+                        pen.Color = e.CellStyle.BackColor;
+                    }
+                }
+            }
+        }
+
+        private (DataGridView? grid, bool valid) ValidateColumn(object? sender, int columnIndex, bool useCurrentCellColumnIndex=false)
+        {
+            DataGridView? s = null;
+            if (sender != null)
+            {
+                if (sender.GetType() == typeof(DataGridView))
+                {
+                    s = (DataGridView)sender;
+                    if(useCurrentCellColumnIndex)
+                    {
+                        columnIndex = s.CurrentCell.ColumnIndex;
+                    }
+                    else if(s.Columns.Count <= columnIndex || columnIndex < 0)
+                    {
+                        return (s, false);
+                    }
+                    if (s.Columns[columnIndex].CellType.Name == typeof(DataGridViewCheckBoxCell).Name && (checkBoxColumnNames.Count > 0 ? checkBoxColumnNames.Contains(s.Columns[columnIndex].Name) : true))
+                    {
+                        return (s, true);
+                    }
+                    //else{return (s, false);}
+                }
+            }
+            return (s, false);
+        }
+
+        public void Deselect(DataGridView sender, string checkBoxColumnName)
+        {
+            if (selectedCell[checkBoxColumnName] > -1)
+            {
+                for(int i = 0; i < sender.Columns.Count; i++) 
+                {
+                    if (ValidateColumn(sender, i).valid)
+                    {
+                        sender.Rows[selectedCell[checkBoxColumnName]].Cells[i].Value = false;
+                        selectedCell[checkBoxColumnName] = -1;
+                    }
+                }
+            }
+        }
+        private void DeselectPreviousOption(object? sender, EventArgs e)
+        //enforces single selection
+        {
+            var valid = ValidateColumn(sender, 0, true);
+            
+            if (valid.valid)
+            {
+                DataGridView s = valid.grid;
+                if (selectedCell[s.Columns[s.CurrentCell.ColumnIndex].Name] != s.CurrentCell.RowIndex && selectedCell[s.Columns[s.CurrentCell.ColumnIndex].Name] > -1)
+                {
+                    Deselect(s, s.Columns[s.CurrentCell.ColumnIndex].Name);
+                }
+                selectedCell[s.Columns[s.CurrentCell.ColumnIndex].Name] = s.CurrentCell.RowIndex;
+            }
+        }
+
+        private void StateCheckOnCellEndEdit(object? sender, DataGridViewCellEventArgs e)
+        //prevents de-selection 
+        {
+            var valid = ValidateColumn(sender, e.ColumnIndex);
+            if (valid.valid)
+            {
+                DataGridView s = valid.grid;
+                if (e.RowIndex == selectedCell[s.Columns[s.CurrentCell.ColumnIndex].Name])
+                {
+                    if (s.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+                    {
+                        if ((bool)s.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != true)
+                        {
+                            s.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = true;
+                            //selectedCell[s.Columns[s.CurrentCell.ColumnIndex].Name] = -1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SearchGrid.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetStatusInfo
+{
+    public delegate void ButtonAction<Object, EventArgs>();
+    internal class SearchGridDecorator
+    {
+        public DataGridView searchableGrid;
+        public DataGridView searchGrid;
+        public Button? searchButton;
+        public Button? searchClearButton;
+        public Action actionsOnSearch;
+        public Func<DataTable> resetSearchableGridData;
+        //magnifying glass painter
+        private int penThickness = 2;
+        private SolidBrush borderColor = new SolidBrush(Color.LightGray);
+        //non-searchable field info
+        private Color nonSearchableBackgroundColor = Color.Gray;
+        private bool nonSearchableHeadersInvisible = true;
+        public List<string> nonSearchableFields = new List<string>();
+
+        public SearchGridDecorator(DataGridView SearchableGrid, DataGridView SearchGrid, Button SearchButton, Button SearchClearButton, Func<DataTable> ResetSearchableGridData) : this(SearchableGrid, SearchGrid, SearchButton, ResetSearchableGridData)
+        {
+            searchClearButton = SearchClearButton;
+            searchClearButton.Click += searchClearButton_Click;
+        }
+        public SearchGridDecorator(DataGridView SearchableGrid, DataGridView SearchGrid, Button SearchButton, Func<DataTable> ResetSearchableGridData) : this(SearchableGrid, SearchGrid, ResetSearchableGridData)
+        {
+            searchButton = SearchButton;
+            searchButton.Click += searchButton_Click;
+        }
+
+        public SearchGridDecorator(DataGridView SearchableGrid, DataGridView SearchGrid, Func<DataTable> ResetSearchableGridData)
+        {
+            searchableGrid = SearchableGrid;
+            searchableGrid.ColumnWidthChanged += MatchColumnWidth;
+            searchableGrid.DataContextChanged += MatchColumnOrder;
+            resetSearchableGridData = ResetSearchableGridData;
+
+            searchGrid = SearchGrid;
+            searchGrid.CellPainting += SearchGrid_PaintMagnifyingGlass;
+            searchGrid.KeyUp += SearchGrid_KeyUp;
+            searchGrid.EnableHeadersVisualStyles = false;
+            actionsOnSearch += executeSearch;
+        }
+
+        public void AddSearchAction(Action SearchAction)
+        {
+            actionsOnSearch += SearchAction;
+        }
+
+        public void RemoveSearchAction(Action SearchAction)
+        {
+            //original
+            if (actionsOnSearch == null ? false : actionsOnSearch.GetInvocationList().Contains(SearchAction))
+            {
+                if (SearchAction != null)
+                {
+                    actionsOnSearch -= SearchAction;
+                }
+            }
+        }
+        public void ChangeMagnifyingGlass(int PenThickness, SolidBrush BorderColor)
+        {
+            penThickness = PenThickness;
+            borderColor = BorderColor;
+        }
+
+        private void MatchColumnOrder(object? sender, EventArgs e)
+        {
+            MatchColumnOrder();
+        }
+        public void MatchColumnOrder()
+        {
+            int missing = 0;
+            foreach (DataGridViewColumn column in searchableGrid.Columns)
+            {
+                if (searchGrid.Columns[column.Name] != null)
+                {
+                    searchGrid.Columns[column.Name].DisplayIndex = column.DisplayIndex - missing;
+                }
+                else
+                {
+                    missing++;
+                }
+            }
+        }
+
+        private void MatchColumnWidth(object? sender, DataGridViewColumnEventArgs e)
+        {
+            MatchColumnWidth();
+        }
+        public void MatchColumnWidth()
+        {
+            var visibleColumns = from DataGridViewColumn c in searchableGrid.Columns
+                                 where c.Visible
+                                 select c;
+            foreach (DataGridViewColumn column in visibleColumns)
+            {
+                if (searchGrid.Columns[column.Name] != null)
+                {
+                    searchGrid.Columns[column.Name].Width = column.Width;
+                }
+            }
+        }
+        public void FormatNonSearchableFields()
+        {
+            foreach (string columnName in nonSearchableFields)
+            {
+                if (searchGrid.Columns.Contains(columnName))
+                {
+                    searchGrid.Columns[columnName].DefaultCellStyle.BackColor = nonSearchableBackgroundColor;
+                    searchGrid.Columns[columnName].HeaderCell.Style.BackColor = nonSearchableBackgroundColor;
+                    if (nonSearchableHeadersInvisible) { searchGrid.Columns[columnName].HeaderText = ""; }
+                    searchGrid.Columns[columnName].ReadOnly = true;
+                }
+            }
+        }
+        public DataSet GridDataSetter(DataSet dataSet, InputConfigurations configs, string tableName = null)
+        {
+            if (tableName == null)
+            {
+                tableName = searchGrid.Name;
+            }
+            return GridDataSetter(searchGrid, dataSet, configs, tableName);
+        }
+        public static DataSet GridDataSetter(DataGridView grid, DataSet? dataSet, InputConfigurations configs, string tableName = null)
+        {
+            //should I make an overload without dataSet?
+            if (tableName == null)
+            {
+                tableName = grid.Name;
+            }
+            if (dataSet == null)
+            {
+                dataSet = new DataSet();
+            }
+            DataTable manifest = dataSet.Tables.Add(tableName);
+            foreach (KeyValuePair<String, InputConfiguration> kvp in configs.configurations)
+            {
+                manifest.Columns.Add(kvp.Key, kvp.Value.typeOf);
+            }
+            grid.DataSource = manifest;
+            return dataSet;
+        }
+        private void SearchGrid_PaintMagnifyingGlass(object? sender, DataGridViewCellPaintingEventArgs e)
+        {
+            DataGridView? s = null;
+            if (sender != null)
+            {
+                if (sender.GetType() == typeof(DataGridView) && e.RowIndex != -1)
+                {
+                    s = (DataGridView)sender;
+                    if (!s.Rows[e.RowIndex].Cells[e.ColumnIndex].ReadOnly)
+                    {
+                        Size size = e.CellBounds.Size;
+                        if (size.Width < size.Height)
+                        {
+                            size.Width = size.Width - 4 * penThickness;
+                            size.Width = (int)(.65 * size.Width);
+                            size.Height = size.Width;
+                        }
+                        else
+                        {
+                            size.Height = size.Height - 4 * penThickness;
+                            size.Height = (int)(.65 * size.Height);
+                            size.Width = size.Height;
+                        }
+                        Point point = new Point(e.CellBounds.X + (e.CellBounds.Width - (int)(1.9 * size.Width)), e.CellBounds.Y + (e.CellBounds.Height - (int)(1.9 * size.Height))); //fix misalignment with interior do
+                        Rectangle rectangle = new Rectangle(point, size);
+                        Point centerPoint = new Point(rectangle.X + (int)(.9*rectangle.Width), rectangle.Y + (int)(.9 * rectangle.Height));
+                        Point bottomRight = new Point(rectangle.X + (int)(1.3 * rectangle.Width), rectangle.Y + (int)(1.3 * rectangle.Height));
+                        e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                        e.Paint(rectangle, DataGridViewPaintParts.All);
+                        Pen pen = new Pen(borderColor, penThickness);
+                        e.Graphics.DrawEllipse(pen, rectangle);
+                        pen.Width = 4;
+                        e.Graphics.DrawLine(pen, centerPoint, bottomRight);
+                        e.Handled = true;
+                    }
+                }
+            }
+        }
+        private void SearchGrid_KeyUp(object? sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                DataGridView? s = null;
+                if (sender != null)
+                {
+                    s = (DataGridView)sender;
+                    s.EndEdit();
+                }
+                actionsOnSearch();
+            }
+        }
+
+        private void searchButton_Click(object? sender, EventArgs e)
+        {
+            actionsOnSearch();
+        }
+
+        private void executeSearch()
+        {
+            DataTable searchableGridDataTable = (DataTable)searchableGrid.DataSource;
+            string queryFilter = "";
+            foreach (DataGridViewCell cell in searchGrid.Rows[0].Cells)
+            {
+                if (cell.Value != null && cell.Value != DBNull.Value)
+                {
+                    if (cell.Value.ToString() != "")
+                    {
+                        queryFilter += $"{cell.OwningColumn.Name} LIKE '{cell.Value}' AND ";
+                    }
+                }
+            }
+            if (queryFilter.Length > 5)
+            {
+                queryFilter = queryFilter.Substring(0, queryFilter.Length - 5);
+                DataTable a = new DataTable();
+                a = searchableGridDataTable.Copy();
+                a.Clear();
+                var b = searchableGridDataTable.Select(queryFilter);
+                foreach (var item in b)
+                {
+                    a.ImportRow(item);
+                }
+                searchableGrid.DataSource = a;
+            }
+        }
+        private void searchClearButton_Click(object? sender, EventArgs e)
+        {
+            foreach (DataGridViewCell cell in searchGrid.Rows[0].Cells)
+            {
+                cell.Value = DBNull.Value;
+            }
+            searchableGrid.DataSource = resetSearchableGridData();
+        }
+    }
+}
